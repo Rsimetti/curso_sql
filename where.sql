b7ad9ded8f167271d6ecdb8ea489a7e364ee9a4b
rsimetti <rsimetti>
2022-12-14 00:59:11 +0000

    usando where

---
+++ where.sql
@@ -0,0 +1,43 @@
+-- Databricks notebook source
+SELECT * 
+FROM silver_olist.pedido
+
+WHERE descSituacao = 'delivered'
+
+-- LIMIT 100
+
+-- COMMAND ----------
+
+SELECT * 
+FROM silver_olist.pedido
+
+WHERE descSituacao = 'canceled'
+
+-- LIMIT 100
+
+-- COMMAND ----------
+
+SELECT * 
+FROM silver_olist.pedido
+
+WHERE (descSituacao = 'shipped' OR descSituacao = 'canceled')
+AND year(dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT * 
+FROM silver_olist.pedido
+
+WHERE descSituacao IN ('shipped', 'canceled')
+AND year(dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT *,
+       datediff(dtEstimativaEntrega, dtAprovado)
+
+FROM silver_olist.pedido
+
+WHERE descSituacao IN ('shipped', 'canceled')
+AND year(dtPedido) = '2018'
+AND datediff(dtEstimativaEntrega, dtAprovado) > 30
